b0d0359c61e99a139a3d28ac06875f611143beb9
Nguyen Viet Cuong <nvcuong92@gmail.com>
2012-08-27 14:26:51 +0800

    Update src/Applications/PuncFeatures/EdgeWord.java

--- src/Applications/PuncFeatures/EdgeWord.java
+++ src/Applications/PuncFeatures/EdgeWord.java
@@ -28,7 +28,7 @@ import HOSemiCRF.*;
  */
 public class EdgeWord extends FeatureType {
 	
-	public ArrayList<String> generateObsAt(DataSequence seq, int segStart, int segEnd) {
+    public ArrayList<String> generateObsAt(DataSequence seq, int segStart, int segEnd) {
         ArrayList<String> obs = new ArrayList<String>();
         if (segStart > 0) {
             obs.add("EW." + seq.x(segStart));
@@ -36,7 +36,7 @@ public class EdgeWord extends FeatureType {
         return obs;
     }
 	
-	public int order() {
+    public int order() {
         return 1;
     }
 }
