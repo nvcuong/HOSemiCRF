03a3ac7bffbdf6bb1e7b2b0c1cc9b65f66cdbbcd
Nguyen Viet Cuong <nvcuong92@gmail.com>
2012-08-27 13:58:32 +0800

    Update src/HOSemiCRF/Loglikelihood.java

--- src/HOSemiCRF/Loglikelihood.java
+++ src/HOSemiCRF/Loglikelihood.java
@@ -28,10 +28,10 @@ public class Loglikelihood {
     double logli; // Loglikelihood value
     double derivatives[]; // Loglikelihood derivatives
 
-	/**
-	 * Construct a loglikelihood with a given number of features.
-	 * @param n Number of features
-	 */
+    /**
+     * Construct a loglikelihood with a given number of features.
+     * @param n Number of features
+     */
     public Loglikelihood(int n) {
         logli = 0;
         derivatives = new double[n];
