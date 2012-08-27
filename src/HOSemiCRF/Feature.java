ef491a2bae4e31c3abe0fe38b495aec3db295d5e
Nguyen Viet Cuong <nvcuong92@gmail.com>
2012-08-27 11:59:38 +0800

    Update src/HOSemiCRF/Feature.java

--- src/HOSemiCRF/Feature.java
+++ src/HOSemiCRF/Feature.java
@@ -26,18 +26,18 @@ package HOSemiCRF;
 public class Feature {
 
     String obs; // The observation part of the feature
-	String pat; // The pattern of the feature
+    String pat; // The pattern of the feature
     double value; // Value of the feature
 
-	/**
-	 * Construct a new feature from observation, pattern, and value.
-	 * @param obs Observation of the feature
-	 * @param pat Label pattern of the feature
-	 * @param value Value of the feature
-	 */
+    /**
+     * Construct a new feature from observation, pattern, and value.
+     * @param obs Observation of the feature
+     * @param pat Label pattern of the feature
+     * @param value Value of the feature
+     */
     public Feature(String obs, String pat, double value) {
-		this.obs = obs;
-		this.pat = pat;
+        this.obs = obs;
+        this.pat = pat;
         this.value = value;
     }
 }
