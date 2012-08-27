fca08a77308f79fc95258087585ed2ba4abd9e41
Nguyen Viet Cuong <nvcuong92@gmail.com>
2012-08-27 11:45:45 +0800

    Update src/Parallel/Schedulable.java

--- src/Parallel/Schedulable.java
+++ src/Parallel/Schedulable.java
@@ -30,23 +30,23 @@ public interface Schedulable {
 	 * @param taskID ID of the task
 	 * @return Partial result
 	 */
-    public Object compute(int taskID); //can do synchronized update here if partial result takes too much memory
+	 public Object compute(int taskID); //can do synchronized update here if partial result takes too much memory
 
 	/**
 	 * Return the total number of tasks.
 	 * @return Total number of tasks
 	 */
-    public int getNumTasks();
+	 public int getNumTasks();
 
 	/**
 	 * Return the next task ID.
 	 * @return Next task ID
 	 */
-    public int fetchCurrTaskID(); // synchronize
+	 public int fetchCurrTaskID(); // synchronize
 
 	/**
 	 * Update using the partial result.
 	 * @param partialResult Partial result
 	 */
-    public void update(Object partialResult); // synchronize
+	 public void update(Object partialResult); // synchronize
 }
