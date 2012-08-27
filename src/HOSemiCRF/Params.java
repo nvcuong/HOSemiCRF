81bbcce26ab5f9d78520fe36ed26ba408e4e4a69
Nguyen Viet Cuong <nvcuong92@gmail.com>
2012-08-27 13:59:28 +0800

    Update src/HOSemiCRF/Params.java

--- src/HOSemiCRF/Params.java
+++ src/HOSemiCRF/Params.java
@@ -31,26 +31,26 @@ public class Params {
     int numLabels; // Number of labels
     int maxIters = 100; // Number of training iterations
     int numthreads = 1; // Number of parallel threads
-	int maxSegment = -1; // Maximum segment length
+    int maxSegment = -1; // Maximum segment length
     double invSigmaSquare = 1.0; // Inverse of Sigma Squared
     double epsForConvergence = 0.001; // Convergence Precision
 
-	/**
-	 * Construct a parameters object.
-	 * @param filename Name of configuration file
-	 * @param nl Number of labels
-	 */
+    /**
+     * Construct a parameters object.
+     * @param filename Name of configuration file
+     * @param nl Number of labels
+     */
     public Params(String filename, int nl) throws IOException {
-		Properties options = new Properties();
+    	Properties options = new Properties();
         options.load(new FileInputStream(filename));
         String value = null;
         if ((value = options.getProperty("maxIters")) != null) {
             maxIters = Integer.parseInt(value);
         }
-		if ((value = options.getProperty("numthreads")) != null) {
+        if ((value = options.getProperty("numthreads")) != null) {
             numthreads = Integer.parseInt(value);
         }
-		if ((value = options.getProperty("maxSegment")) != null) {
+        if ((value = options.getProperty("maxSegment")) != null) {
             maxSegment = Integer.parseInt(value);
         }
         if ((value = options.getProperty("invSigmaSquare")) != null) {
@@ -59,7 +59,6 @@ public class Params {
         if ((value = options.getProperty("epsForConvergence")) != null) {
             epsForConvergence = Double.parseDouble(value);
         }
-		
-		numLabels = nl;
+        numLabels = nl;
     }
 }
