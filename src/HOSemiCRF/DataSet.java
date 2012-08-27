b8c61d9cc9de41fc4937bb2172d2c278b79a52e5
Nguyen Viet Cuong <nvcuong92@gmail.com>
2012-08-27 11:58:37 +0800

    Update src/HOSemiCRF/DataSet.java

--- src/HOSemiCRF/DataSet.java
+++ src/HOSemiCRF/DataSet.java
@@ -30,26 +30,26 @@ public class DataSet {
 
     ArrayList<DataSequence> trainSeqs; // List of all data sequences
 
-	/**
-	 * Construct a dataset from a list of data sequences.
-	 * @param trs List of data sequences
-	 */
+    /**
+     * Construct a dataset from a list of data sequences.
+     * @param trs List of data sequences
+     */
     public DataSet(ArrayList<DataSequence> trs) {
         trainSeqs = trs;
     }
 
-	/**
-	 * Get the list of data sequences in the dataset.
-	 * @return List of data sequences
-	 */
+    /**
+     * Get the list of data sequences in the dataset.
+     * @return List of data sequences
+     */
     public ArrayList<DataSequence> getSeqList() {
         return trainSeqs;
     }
 
-	/**
-	 * Write the dataset to a file.
-	 * @param filename Name of the output file
-	 */
+    /**
+     * Write the dataset to a file.
+     * @param filename Name of the output file
+     */
     public void writeToFile(String filename) throws Exception {
         BufferedWriter bw = new BufferedWriter(new FileWriter(filename, false));
         for (int i = 0; i < trainSeqs.size(); i++) {
