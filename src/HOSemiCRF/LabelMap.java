aff47e7de6b123a90acf4f333514710ac2c3829b
Nguyen Viet Cuong <nvcuong92@gmail.com>
2012-08-27 13:48:10 +0800

    Update src/HOSemiCRF/LabelMap.java

--- src/HOSemiCRF/LabelMap.java
+++ src/HOSemiCRF/LabelMap.java
@@ -30,27 +30,27 @@ public class LabelMap {
 
     ArrayList<String> mapTable; // List of labels
 
-	/**
-	 * Construct an empty label map.
-	 */
+    /**
+     * Construct an empty label map.
+     */
     public LabelMap() {
         mapTable = new ArrayList<String>();
     }
 
-	/**
-	 * Return the size of the map.
-	 * @return Number of labels
-	 */
+    /**
+     * Return the size of the map.
+     * @return Number of labels
+     */
     public int size() {
         return mapTable.size();
     }
 
-	/**
-	 * Return the index of a label.
-	 * If there is no such label, add it to the map and return its index.
-	 * @param labelStr Input label
-	 * @return Index of the input label
-	 */
+    /**
+     * Return the index of a label.
+     * If there is no such label, add it to the map and return its index.
+     * @param labelStr Input label
+     * @return Index of the input label
+     */
     public int map(String labelStr) {
         int index = mapTable.indexOf(labelStr);
         if (index == -1) {
@@ -60,20 +60,20 @@ public class LabelMap {
         return index;
     }
 
-	/**
-	 * Return the label with a given index.
-	 * @param l Index of the label
-	 * @return The label string
-	 */
+    /**
+     * Return the label with a given index.
+     * @param l Index of the label
+     * @return The label string
+     */
     public String revMap(int l) {
         return mapTable.get(l);
     }
 
-	/**
-	 * Map labels into their indices.
-	 * @param labelStrList List of label strings
-	 * @return List of indices of the input labels
-	 */
+    /**
+     * Map labels into their indices.
+     * @param labelStrList List of label strings
+     * @return List of indices of the input labels
+     */
     public int[] mapArrayList(ArrayList<String> labelStrList) {
         int[] result = new int[labelStrList.size()];
         for (int i = 0; i < result.length; i++) {
@@ -82,23 +82,23 @@ public class LabelMap {
         return result;
     }
 	
-	/**
-	 * Map label indices into label strings.
-	 * @param labels Label array
-	 * @return Label string array
-	 */
-	public String[] revArray(int[] labels) {
-		String[] result = new String[labels.length];
-		for (int i = 0; i < labels.length; i++) {
-			result[i] = revMap(labels[i]);
-		}
-		return result;
-	}
-
-	/**
-	 * Write the label map into a file.
-	 * @param filename Name of the output file
-	 */
+    /**
+     * Map label indices into label strings.
+     * @param labels Label array
+     * @return Label string array
+     */
+    public String[] revArray(int[] labels) {
+        String[] result = new String[labels.length];
+        for (int i = 0; i < labels.length; i++) {
+            result[i] = revMap(labels[i]);
+        }
+        return result;
+    }
+    
+    /**
+     * Write the label map into a file.
+     * @param filename Name of the output file
+     */
     public void write(String filename) throws IOException {
         PrintWriter out = new PrintWriter(new FileOutputStream(filename));
         out.println(mapTable.size());
@@ -107,11 +107,11 @@ public class LabelMap {
         }
         out.close();
     }
-
-	/**
-	 * Read the label map from a file.
-	 * @param filename Name of the input file
-	 */
+    
+    /**
+     * Read the label map from a file.
+     * @param filename Name of the input file
+     */
     public void read(String filename) throws IOException {
         BufferedReader in = new BufferedReader(new FileReader(filename));
         int size = Integer.parseInt(in.readLine());
