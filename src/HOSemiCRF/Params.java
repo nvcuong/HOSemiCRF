b6971b449af1ef70445acdb15bd691c462d227d1
tndoan <doan.thanh.nam.1988@gmail.com>
2015-07-28 14:28:51 +0800

    add learning rate as parameters of file

--- src/HOSemiCRF/Params.java
+++ src/HOSemiCRF/Params.java
@@ -25,6 +25,7 @@ import java.util.*;
 /**
  * Parameters class
  * @author Nguyen Viet Cuong
+ * @author tndoan
  */
 public class Params {
 
@@ -34,6 +35,11 @@ public class Params {
     int maxSegment = -1; // Maximum segment length
     double invSigmaSquare = 1.0; // Inverse of Sigma Squared
     double epsForConvergence = 0.001; // Convergence Precision
+    
+    /**
+     * learning rate for first order minimizer
+     */
+    double learningRate = 0.1; // 
 
     /**
      * Construct a parameters object.
@@ -58,6 +64,9 @@ public class Params {
         }
         if ((value = options.getProperty("epsForConvergence")) != null) {
             epsForConvergence = Double.parseDouble(value);
+        }
+        if ((value = options.getProperty("learningRate")) != null){
+        	learningRate = Double.parseDouble(value);
         }
         numLabels = nl;
     }
@@ -108,5 +117,13 @@ public class Params {
 	 */
 	public double getEpsForConvergence() {
 		return epsForConvergence;
+	}
+
+	/**
+	 * 
+	 * @return the value of learning rate
+	 */
+	public double getLearningRate() {
+		return learningRate;
 	}
 }
